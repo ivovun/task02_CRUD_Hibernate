d3725904ede50166d6e8ae79cd56df13fad86a8f
Vladimir Filippov <sergeiych@gmail.com>
2019-11-18 18:12:00 +0300

    after all mentions of JDBC was deleted

--- src/main/java/service/UserServiceImpl.java
+++ src/main/java/service/UserServiceImpl.java
@@ -2,15 +2,11 @@ package service;
 
 import dao.UserDao;
 import dao.UserHibernateDAO;
-import dao.UserJdbcDAO;
 import exception.DBException;
 import model.User;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import util.DBHelper;
-import util.PropertyReader;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
@@ -22,27 +18,15 @@ public class UserServiceImpl implements UserService {
         this.sessionFactory = sessionFactory;
     }
 
-    private static boolean usingJDBC() {
-        return PropertyReader.getProperty("if_JDBC_Then_1_if_Hibernate_Then_2").equals("1");
-    }
-
     public static UserServiceImpl getInstance() {
         if (userServiceImpl == null) {
-            if (usingJDBC()) {
-                userServiceImpl = new UserServiceImpl(  null);
-            } else {
-                userServiceImpl = new UserServiceImpl(DBHelper.getSessionFactory(new Class[] {User.class}));
-            }
+            userServiceImpl = new UserServiceImpl(DBHelper.getSessionFactory(new Class[]{User.class}));
         }
         return userServiceImpl;
     }
 
-    private static UserDao getUserDaoImpl() throws DBException {
-        if (usingJDBC()) {
-            return new UserJdbcDAO(DBHelper.getConnection());
-        } else  {
-            return new UserHibernateDAO(getInstance().getSessionFactory().openSession());
-        }
+    private static UserDao getUserDaoImpl()  {
+        return new UserHibernateDAO(getInstance().getSessionFactory().openSession());
     }
 
     private SessionFactory getSessionFactory() {
